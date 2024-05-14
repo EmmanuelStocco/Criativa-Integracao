9d33b0efccb404f627cebe0a3e2f7509e70bd927
Jesse Satlei <jessesatlei@hotmail.com>
2024-05-14 10:21:20 -0300

    feat: add new configs for project

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import routes from './routes/index';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use('/', routes);
 
